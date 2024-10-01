c5102715451c3b2104213c101ca3f8bd6f05ec9f
LDanielOchoa <daniel.ochoaa.cass@gmail.com>
2024-10-01 14:11:36 -0500

    Cambios realziados por daniel

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,8 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  preview: {
+    port: process.env.PORT || 4173,
+    host: '0.0.0.0',
+  }
 })
