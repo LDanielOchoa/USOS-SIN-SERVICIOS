069ab011c9d8d2b680654f0f03f6967db67480ee
LDanielOchoa <daniel.ochoaa.cass@gmail.com>
2024-10-01 14:42:08 -0500

    Cambios realziados por daniel

--- src/components/FileUploader.vue
+++ src/components/FileUploader.vue
@@ -87,7 +87,7 @@ export default {
       formData.append('usos', this.files.usos);
 
       try {
-        const response = await axios.post('http://sao6.up.railway.app/upload', formData);
+        const response = await axios.post('http://sao66.up.railway.app/upload', formData);
         this.taskId = response.data.task_id;
         this.taskStatus = 'PROGRESS'; // Cambiamos el estado a PROGRESS
 
